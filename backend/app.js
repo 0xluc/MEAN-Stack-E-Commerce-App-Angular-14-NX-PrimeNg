df61c486cf51df15bbe31fd093b94bcd7ac4eae3
0xluc <lucasbrodrigues14@gmail.com>
2023-06-26 00:11:41 -0300

    setting up database

--- backend/app.js
+++ backend/app.js
@@ -5,10 +5,18 @@ var mysql = require('mysql')
 require('dotenv/config')
 
 const connection = mysql.createConnection({
-    
+    host: process.env.DB_URL,
+    port: process.env.DB_PORT,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    socketPath: process.env.DB_SOCKET
 })
 
-
+connection.connect(function(err) {
+    if (err) throw err;
+    console.log("mysql connected");
+})
 
 const api = process.env.API_URL
 
