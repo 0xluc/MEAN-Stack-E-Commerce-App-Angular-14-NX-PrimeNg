1a430cd46f4c567d29566df50940dbef9e1613cc
Lucas Mourão <lucasbrodrigues14@gmail.com>
2023-06-27 17:00:42 -0300

    post in products

--- backend/app.js
+++ backend/app.js
@@ -25,10 +25,30 @@ app.use(morgan('tiny'))
 
 app.get(`${api}/products`, getAllProducts)
 
-app.post(`${api}/products`, (req, res) => {
-    const newProduct = req.body
+app.post(`${api}/products`, async (req, res) => {
+    const product = {
+        name: req.body.req,
+        image: req.body.image,
+        brand: req.body.image,
+        price: req.body.image,
+        rating: req.body.rating,
+        numReviews: req.body.numReviews,
+        isFeatured: req.body.isFeatured,
+        description: req.body.description,
+        category: req.body.category,
+        reviews: req.body.reviews,
+        countInStock: req.body.countInStock,
+        richDescription: req.body.richDescription,
+        images: req.body.images
+    }
     console.log(newProduct) 
-    res.send(newProduct)
+    await Product.create(product).then((productAdded) => {
+        console.log("Added product id:", productAdded.id)
+        res.send(newProduct)
+    }).catch((error) =>{
+        console.log(error)
+        res.status(500).json({error: 'Internal server error'})
+    })
 })
 
 app.listen(3000, () => {
