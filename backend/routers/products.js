e80b00e05e22b692817382870e4c8d4f79dc12d8
Lucas Mourão <lucasbrodrigues14@gmail.com>
2023-07-04 16:57:44 -0300

    images upload added

--- backend/routers/products.js
+++ backend/routers/products.js
@@ -6,18 +6,29 @@ const Product = require('../models/product')
 const Category = require('../models/categories')
 const multer = require('multer')
 
+const FILE_TYPE_MAP = {
+    'image/png': 'png',
+    'image/jpeg': 'jpeg',
+    'image/jpg': 'jpg'
+}
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
-    cb(null, 'public/uploads')
+    const isValidType = FILE_TYPE_MAP[file.mimetype]
+    let uploadError = new Error('Invalid image type.')
+    if (isValidType) {
+      uploadError = null
+    }
+    cb(uploadError, 'public/uploads')
   },
   filename: function (req, file, cb) {
+    const extension = FILE_TYPE_MAP[file.mimetype]
     const fileName = file.originalname.split(' ').join('_')
     const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9)
-    cb(null, fileName + '-' + uniqueSuffix)
+    cb(null, uniqueSuffix + '-' + fileName + '.' + extension)
   }
 })
 
-const upload = multer({ storage: storage })
+const uploadOptions = multer({ storage: storage })
 const getAllProducts = async (req, res) => {
     if(req.query.categories){
         const filter = req.query.categories.split(',')
@@ -55,7 +66,7 @@ const getAllProducts = async (req, res) => {
 
 
 router.get(`/`, getAllProducts)
-router.post(`/`, async (req, res) => {
+router.post(`/`, uploadOptions.single('image'),async (req, res) => {
     const category = await Category.findOne({
         where: {
             id: req.body.category
@@ -64,9 +75,13 @@ router.post(`/`, async (req, res) => {
     if(!category){
         res.status(404).json({error: 'category not found'})
     }
+    const file = req.file
+    if(!file) return res.status(400).send('No file')
+    const fileName  = req.file.filename
+    const basePath = `${req.protocol}://${req.get('host')}/public/uploads/${fileName}`
     const product = {
         name: req.body.req,
-        image: req.body.image,
+        image: basePath,
         brand: req.body.brand,
         price: req.body.price,
         rating: req.body.rating,
@@ -154,6 +169,41 @@ router.put('/:id', async (req, res) => {
         }
         
     } catch (error) {
+        console.log(error)
+        res.status(500).json({error: 'Internal server error'})
+    }
+})
+
+router.put('/gallery-images/:id', uploadOptions.array('images', 10),async (req, res) => {
+    try{
+        const order = await Order.findOne({
+            where: {
+                id: req.params.id
+            }
+        })
+        if(!order){
+            return res.status(404).json({error: 'product not found'})
+        }
+        const files = req.files
+        if(files){
+            files.map((file) => {
+                imagesPaths.push(`${req.protocol}://${req.get('host')}/public/uploads/${file.filename}`)
+            })
+        }
+        let imagesPaths = []
+        const updatedProduct = {
+            images: imagesPaths
+        }
+        const product = await Product.update(updatedProduct, {
+            where: {
+                id: req.params.id
+            }
+        })
+        if(!product){
+            return res.status(404).json({error: 'product not found'})
+        }
+        res.status(200).json(updatedProduct)
+    }catch (error) {
         console.log(error)
         res.status(500).json({error: 'Internal server error'})
     }
