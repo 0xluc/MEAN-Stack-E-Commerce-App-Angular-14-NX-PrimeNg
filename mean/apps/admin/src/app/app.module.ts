fc8e139ed8302dad4e3bfdd66ea77fe062f90888
0xluc <lucasbrodrigues14@gmail.com>
2023-07-09 23:07:18 -0300

    setting up catgories table and removing jwt auth for testing

--- mean/apps/admin/src/app/app.module.ts
+++ mean/apps/admin/src/app/app.module.ts
@@ -7,12 +7,20 @@ import { NxWelcomeComponent } from './nx-welcome.component';
 import { DashboardComponent } from './pages/dashboard/dashboard.component';
 import { ShellComponent } from './shared/shell/shell.component';
 import { SidebarComponent } from './shared/sidebar/sidebar.component';
-
+import { CategoriesListComponent } from './categories/categories-list/categories-list.component';
+import { CardModule } from 'primeng/card';
+import { ToolbarModule } from 'primeng/toolbar';
+import { ButtonModule } from 'primeng/button';
+import { TableModule } from 'primeng/table';
 @NgModule({
-  declarations: [AppComponent, NxWelcomeComponent, DashboardComponent, ShellComponent, SidebarComponent],
+  declarations: [AppComponent, NxWelcomeComponent, DashboardComponent, ShellComponent, SidebarComponent, CategoriesListComponent],
   imports: [
     BrowserModule,
     RouterModule.forRoot(appRoutes, { initialNavigation: 'enabledBlocking' }),
+    CardModule,
+    ToolbarModule,
+    ButtonModule,
+    TableModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
---
+++ mean/apps/admin/src/app/categories/categories-list/categories-list.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'mean-categories-list',
+  templateUrl: './categories-list.component.html',
+  styles: [
+  ]
+})
+export class CategoriesListComponent {
+  categories = [
+
+  ]
+
+}
