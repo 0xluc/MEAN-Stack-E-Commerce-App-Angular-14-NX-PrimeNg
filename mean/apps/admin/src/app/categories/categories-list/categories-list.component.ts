eb013dc83b60a5c0c5ed55a2be654685b3d2e68a
0xluc <lucasbrodrigues14@gmail.com>
2023-07-11 07:43:31 -0300

    delete category

--- mean/apps/admin/src/app/categories/categories-list/categories-list.component.ts
+++ mean/apps/admin/src/app/categories/categories-list/categories-list.component.ts
@@ -1,5 +1,7 @@
 import { CategoriesService, Category } from '@mean/products';
 import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+import { MessageService } from 'primeng/api';
 
 @Component({
   selector: 'mean-categories-list',
@@ -13,12 +15,31 @@ export class CategoriesListComponent implements OnInit{
 
   constructor(
     private categoriesService: CategoriesService,
+    private messageService: MessageService
   ) { }
   ngOnInit(): void {
     this.categoriesService.getCategories().subscribe((categories) =>{
       this.categories = categories
-    } 
-    )      
+    }
+                                                    )
+  }
+  deleteCategory(id: string): Observable<Object>{
+    this.categoriesService.deleteCategory(id).subscribe(() =>{
+      this.messageService.add({
+        severity: 'success',
+        summary: 'Successful',
+        detail: 'Category Deleted',
+        life: 3000
+      })
+    }, (error:any) =>{
+      this.messageService.add({
+        severity: 'error',
+        summary: 'Error',
+        detail: error,
+        life: 3000
+      })
+    })
+
   }
 
-}
+
--- mean/libs/products/src/lib/services/categories.service.ts
+++ mean/libs/products/src/lib/services/categories.service.ts
@@ -8,12 +8,15 @@ import { Observable } from 'rxjs';
 export class CategoriesService {
 
   apiUrl = 'http://localhost:3000/api/v1/categories';
-  constructor( private http: HttpClient) { 
+  constructor( private http: HttpClient) {
   }
   getCategories(): Observable<Category[]> {
     return this.http.get<Category[]>(this.apiUrl);
   }
   createCategory(category: Category): Observable<Category> {
     return this.http.post<Category>(this.apiUrl, category);
+  }
+  deleteCategory(id: string): Observable<Object> {
+    return this.http.delete<Category>(`${this.apiUrl}/${id}`);
   }
 }
